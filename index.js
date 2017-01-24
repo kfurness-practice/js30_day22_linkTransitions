11ea354abd859e55ebda6dafa24a6c1b8a862807
kelly <kelly.n.waite@gmail.com>
2017-01-24 11:23:38 -0700

    Init repo; Grab all of links to add highlight class

---
+++ index.js
@@ -0,0 +1,9 @@
+    // 👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀
+const triggers = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+highlight.classList.add('hightlight');
+document.body.append(highlight);
+
+const highlightLinik = () => {
+  console.log('highlight ☀️');
+}
