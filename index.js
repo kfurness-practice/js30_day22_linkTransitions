7690cdf0a97d6e69079a9b0e0569795d12aac876
kelly <kelly.n.waite@gmail.com>
2017-01-24 11:47:50 -0700

    Set where position where highlight object should be

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
     // 👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀
 const triggers = document.querySelectorAll('a');
 const highlight = document.createElement('span');
-highlight.classList.add('hightlight');
+highlight.classList.add('highlight');
 document.body.append(highlight);
 
-const highlightLinik = () => {
-  console.log('highlight ☀️');
+// const highlightLink = () => {
+//   console.log(this);
+//   const linkCoords = this.getBoundingClientRect();
+//   console.log(linkCoords);
+// }
+
+function highlightLink() {
+    const linkCoords = this.getBoundingClientRect();
+    const coords = {
+      width: linkCoords.width,
+      height: linkCoords.height,
+      top: linkCoords.top + window.scrollY,
+      left: linkCoords.left + window.scrollX
+    }
+    highlight.style.width = `${coords.width}px`;
+    highlight.style.height = `${coords.height}px`;
+    highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
 }
+
+triggers.forEach( a => a.addEventListener('mouseenter', highlightLink));
